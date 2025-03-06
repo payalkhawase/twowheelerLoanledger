d7f02d1247578c7feb8f3ae6fc2e52c738deb639
payalkhawase <payal.khawase14@gmail.com>
2025-03-06 22:47:46 +0530

    add

--- twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/serviceimpl/LedgerServiceImpl.java
+++ twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/serviceimpl/LedgerServiceImpl.java
@@ -1,8 +1,12 @@
 package in.shriram.dreambiketwowheelerloan.ledger.serviceimpl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import in.shriram.dreambiketwowheelerloan.ledger.model.Ledger;
 import in.shriram.dreambiketwowheelerloan.ledger.repository.LedgerRepository;
 import in.shriram.dreambiketwowheelerloan.ledger.servicei.LedgerServiceI;
 @Service
@@ -10,4 +14,10 @@ public class LedgerServiceImpl implements LedgerServiceI{
 
 	@Autowired
 	LedgerRepository lr;
+
+	@Override
+	public Set<Ledger> addData(int customerId) {
+		
+		return null;
+	}
 }
