61cecc5bc4e3e95711adb515d67b6e4a1a834d5f
payalkhawase <payal.khawase14@gmail.com>
2025-03-08 12:02:06 +0530

    add

--- twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/repository/LedgerRepository.java
+++ twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/repository/LedgerRepository.java
@@ -12,6 +12,9 @@ public interface LedgerRepository extends JpaRepository<Ledger, Integer>{
 	//@Query(value = "SELECT SUM(payableAmountwithInterest) FROM Ledger WHERE customerId", nativeQuery = true) 
 	//public double selectPayableAmountwithInterest();
 	
-	@Query(value = "SELECT SUM(payable_amount_with_interest) FROM Ledger WHERE customer_id = :customerId", nativeQuery = true)
-	Double selectPayableAmountwithInterest(@Param("customerId") int customerId);
+	//@Query(value = "SELECT SUM(payable_amount_with_interest) FROM Ledger WHERE customer_id = :customerId", nativeQuery = true)
+	//Double selectPayableAmountwithInterest(@Param("customerId") int customerId);
+	
+	@Query(value="SELECT SUM(payable_amountwith_interest) FROM  twowheelerloan.ledger inner join  twowheelerloan.customer_led on ledger.ledger_id=customer_led.led_ledger_id where customer_led.customer_customer_id=1 and ledger.previous_emit_status='paid'",nativeQuery = true)
+	public Double selectPayableAmountwithInterest( );
 }
