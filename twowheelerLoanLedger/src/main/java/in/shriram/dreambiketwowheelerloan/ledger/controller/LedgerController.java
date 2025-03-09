03eb68678da3a88741dbc0d13f795b7484df33ed
payalkhawase <payal.khawase14@gmail.com>
2025-03-09 15:10:40 +0530

    add

--- twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/controller/LedgerController.java
+++ twowheelerLoanLedger/src/main/java/in/shriram/dreambiketwowheelerloan/ledger/controller/LedgerController.java
@@ -1,7 +1,5 @@
 package in.shriram.dreambiketwowheelerloan.ledger.controller;
 
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.shriram.dreambiketwowheelerloan.ledger.model.Customer;
-import in.shriram.dreambiketwowheelerloan.ledger.model.Ledger;
 import in.shriram.dreambiketwowheelerloan.ledger.servicei.LedgerServiceI;
 
 @RestController
